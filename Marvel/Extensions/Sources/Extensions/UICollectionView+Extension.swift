5a1cdfc0eb489b4b1379b4d0d12950975e57f931
Ali Hasanoğlu <hasanogliali80@gmail.com>
2021-09-21 22:47:58 +0300

    CollectionView extensions added.

---
+++ Marvel/Extensions/Sources/Extensions/UICollectionView+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UICollectionView+Extension.swift
+//  
+//
+//  Created by Ali Hasanoğlu on 20.09.2021.
+//
+
+import UIKit
+
+public extension UICollectionView {
+    
+    func registerNib(_ type: UICollectionViewCell.Type, bundle: Bundle) {
+        register(
+            UINib(nibName: type.cellIdentifier, bundle: bundle),
+            forCellWithReuseIdentifier: type.cellIdentifier
+        )
+    }
+    
+    func dequeueCell<CellType: UICollectionViewCell>(type: CellType.Type, indexPath: IndexPath) -> CellType {
+        guard let cell = dequeueReusableCell(
+            withReuseIdentifier: CellType.cellIdentifier, for: indexPath
+        ) as? CellType else {
+            fatalError("Wrong type of cell \(type)")
+        }
+        return cell
+    }
+}
